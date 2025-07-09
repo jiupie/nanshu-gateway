ef4df24431945d4239265ded509741b6562ae320
南顾北衫\ngbs <17674641491@163.com>
2025-07-10 00:19:10 +0800

    更改 grpc-spring

--- nanshu-gateway-comet/src/main/java/com/ngbs/nanshu/gateway/comet/rpc/CometMsgImpl.java
+++ nanshu-gateway-comet/src/main/java/com/ngbs/nanshu/gateway/comet/rpc/CometMsgImpl.java
@@ -3,11 +3,13 @@ package com.ngbs.nanshu.gateway.comet.rpc;
 import com.ngbs.nanshu.gateway.comet.api.CometMsgGrpc;
 import com.ngbs.nanshu.gateway.comet.api.CometService;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 /**
  * @author 南顾北衫
  * @date 2024/8/24
  */
+@GrpcService
 public class CometMsgImpl extends CometMsgGrpc.CometMsgImplBase {
     @Override
     public void pushMsg(CometService.PushMsgReq request, StreamObserver<CometService.PushMsgResponse> responseObserver) {
--- nanshu-gateway-logic/src/main/java/com/ngbs/nanshu/gateway/logic/GatewayLogicApplication.java
+++ nanshu-gateway-logic/src/main/java/com/ngbs/nanshu/gateway/logic/GatewayLogicApplication.java
@@ -1,6 +1,5 @@
 package com.ngbs.nanshu.gateway.logic;
 
-import com.nanshu.grpc.nacos.EnableGrpcNacos;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,7 +8,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @author 南顾北衫
  * @date 2024/8/11
  */
-@EnableGrpcNacos
 @SpringBootApplication
 public class GatewayLogicApplication {
     public static void main(String[] args) {
