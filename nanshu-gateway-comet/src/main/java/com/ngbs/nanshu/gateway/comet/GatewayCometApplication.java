636bbc701212ee2e9920a6bb2806f881eef81bed
南顾北衫\ngbs <17674641491@163.com>
2025-07-23 23:43:44 +0800

    add spring cloud

--- nanshu-gateway-comet/src/main/java/com/ngbs/nanshu/gateway/comet/GatewayCometApplication.java
+++ nanshu-gateway-comet/src/main/java/com/ngbs/nanshu/gateway/comet/GatewayCometApplication.java
@@ -3,11 +3,13 @@ package com.ngbs.nanshu.gateway.comet;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 南顾北衫
  * @date 2024/8/8
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayCometApplication {
     public static void main(String[] args) {
